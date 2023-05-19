4eac0b66558c4fe7d8f50c288608fd26f882fcdc
Mohamad Arga <arga@mmi-pt.com>
2023-05-20 00:52:51 +0700

    Update BDD

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'r7neio',
   e2e: {
     setupNodeEvents(on, config) {
       on('file:preprocessor',cucumber())
